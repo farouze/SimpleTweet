b01722335822bd0d8bed99af87650308698d521b
farouze <73491297+farouze@users.noreply.github.com>
2021-09-23 14:56:13 -0400

    new files

--- app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -45,7 +46,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	@Override
 	public void onLoginSuccess() {
 		Log.i("rkprkp", "login success");
-		// Intent i = new Intent(this, PhotosActivity.class);
+		 Intent i = new Intent(this, TimelineActivity.class);
 		// startActivity(i);
 	}
 
